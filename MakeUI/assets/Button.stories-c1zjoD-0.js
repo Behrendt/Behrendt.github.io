92b59b172b2f3f08c348c8bf3ae5e209fef8747f
Josh Behrendt <josh.behrendt@papercut.com>
2025-02-27 14:41:35 +1100

    added makeUI

---
+++ MakeUI/assets/Button.stories-c1zjoD-0.js
@@ -0,0 +1,16 @@
+import{j as e,B as s}from"./Chip-D2J0hXHu.js";import"./index-CecxcEPL.js";const l={title:"Design System/Components/Button",component:s,argTypes:{variation:{options:["primary","secondary"],control:{type:"select"}},size:{options:["small","medium","large"],control:{type:"radio"}},handleClick:{action:"clicked"}}},t={args:{variation:"primary",disabled:!1,size:"medium",label:"Button"},render:a=>e.jsxs("div",{style:{display:"flex",justifyContent:"center",width:"100%"},children:[" ",e.jsx(s,{...a})]})};var n,o,r;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    variation: "primary",
+    disabled: false,
+    size: "medium",
+    label: "Button"
+  },
+  render: (props: ButtonProps) => <div style={{
+    display: "flex",
+    justifyContent: "center",
+    width: "100%"
+  }}>
+      {" "}
+      <ButtonStory {...props} />
+    </div>
+}`,...(r=(o=t.parameters)==null?void 0:o.docs)==null?void 0:r.source}}};const p=["Button"];export{t as Button,p as __namedExportsOrder,l as default};
---
+++ MakeUI/assets/Chip.stories-Cu7xB9Oy.js
@@ -0,0 +1,14 @@
+import{j as s,C as i}from"./Chip-D2J0hXHu.js";import"./index-CecxcEPL.js";const d={title:"Design System/Components/Chip",component:i},e={args:{size:"medium",label:"Chip"},render:o=>s.jsxs("div",{style:{display:"flex",justifyContent:"center",width:"100%"},children:[" ",s.jsx(i,{...o})," "]})};var n,r,t;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    size: "medium",
+    label: "Chip"
+  },
+  render: (props: ChipProps) => <div style={{
+    display: "flex",
+    justifyContent: "center",
+    width: "100%"
+  }}>
+      {" "}
+      <ChipStory {...props} />{" "}
+    </div>
+}`,...(t=(r=e.parameters)==null?void 0:r.docs)==null?void 0:t.source}}};const m=["Chip"];export{e as Chip,m as __namedExportsOrder,d as default};
